32d845fb994ceb0913c2ae8ff8308e6631a2b8ed
Christine Oo <ootjinvoon@gmail.com>
2020-08-27 16:01:09 +0900

    temp9

---
+++ job_status.sh
@@ -0,0 +1,26 @@
+SECONDS=0
+TIMEOUT=360
+expected_state='"SUCCEEDED"'
+auth_token="$(gcloud auth print-access-token)"
+state=$(curl -X GET \
+	-H "Content-Type: application/json" \
+	-H "Authorization: Bearer $auth_token" \
+	"https://content-ml.googleapis.com/v1/projects/$1/jobs/$2" | jq .state)
+
+while [[ $SECONDS -lt $TIMEOUT ]]
+do
+  if [ "$state" == "$expected_state" ]; then
+    echo "Job succeeded! All done!"
+    break
+  else
+    echo "Got $state :( Not done yet..."
+  fi
+
+  ((SECONDS++))
+  sleep 120
+done
+
+if [ $SECONDS -gt $TIMEOUT ]; then
+    echo "TIMEOUT reached.."
+    exit 1
+fi
